d83e1a928e50c820c4ee2b731fc0ba0661f9453d
Mário Ponte <ponteolavo30@gmail.com>
2023-10-04 21:19:24 +0100

    fixing bug

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
                         Projects
                     </div>
                     <p className="text-lg mt-8">
-                        Here's some of the work I've done recently. Each project presented is a sample of the kind of solutions I'm capable of creating. 
+                        Here&apos;s some of the work I&apos;ve done recently. Each project presented is a sample of the kind of solutions I&apos;m capable of creating. 
                         As well as presenting the projects, I also describe the solutions proposed and the challenges I faced. I hope you find 
                         inspiration in my work and see how my skills and knowledge can be useful for your own projects.
                     </p>
