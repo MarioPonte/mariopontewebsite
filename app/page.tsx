1b6f73d0d0825885b805c709f382a7032dd50537
Mário Ponte <ponteolavo30@gmail.com>
2023-10-25 17:59:24 +0100

    solving hygraph bug

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,10 @@ const getPageData = async (): Promise<HomePageData> => {
     }
   `
 
-  return fetchHygraphQuery(query, 60 * 60 * 24);
+  return fetchHygraphQuery(
+    query, 
+    1000 * 60 * 60 * 24, // 1 day
+  );
 }
 
 export default async function Home() {
